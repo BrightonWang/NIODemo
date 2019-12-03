364e094e92ee45f4c9f208dafe22b01abd0ef319
wangxiaoliang <15618359568@163.com>
2019-12-03 13:43:52 +0800

    Netty  basic

--- nettyDemo/src/main/java/com/brighton/netty/basic/NettyServer.java
+++ nettyDemo/src/main/java/com/brighton/netty/basic/NettyServer.java
@@ -8,6 +8,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.sctp.nio.NioSctpServerChannel;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 /**
  * <p>
@@ -17,20 +18,20 @@ import io.netty.channel.socket.SocketChannel;
  * @version :  15:44 2019-11-14 Brigh Exp $
  */
 public class NettyServer {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         //1. 创建一个线程组: 接收客户端连接
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         //2. 创建一个线程组: 处理网络操作
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
         //3. 创建服务器端启动助手来配置参数
         ServerBootstrap b = new ServerBootstrap();
-        b.group(bossGroup,workerGroup) //4. 设置两个线程组
-                .channel(NioSctpServerChannel.class) //5. 使用NioServerSocketChannel作为服务器端通道的实现
-                .option(ChannelOption.SO_BACKLOG,128) //6. 设置线程队列中等待的个数
-                .childOption(ChannelOption.SO_KEEPALIVE,true) //7.保存活动连接状态
+        b.group(bossGroup, workerGroup) //4. 设置两个线程组
+                .channel(NioServerSocketChannel.class) //5. 使用NioServerSocketChannel作为服务器端通道的实现
+                .option(ChannelOption.SO_BACKLOG, 128) //6. 设置线程队列中等待的个数
+                .childOption(ChannelOption.SO_KEEPALIVE, true) //7.保存活动连接状态
                 .childHandler(new ChannelInitializer<SocketChannel>() { //8. 创建一个通道初始化对象
                     @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {  //9. 往Pipeline链中添加自定义的handler类
+                    public void initChannel(SocketChannel socketChannel) {  //9. 往Pipeline链中添加自定义的handler类
                         socketChannel.pipeline().addLast(new NettyServerHandler());
                     }
                 });
